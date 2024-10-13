69ff624ada540618a117555fd82ed3f72a8a2a03
ChristianArguelles <bonechanbuto@gmail.com>
2024-10-14 07:57:51 +0800

    name

--- About.js
+++ About.js
@@ -4,7 +4,7 @@ import { Container } from 'react-bootstrap';
 const About = () => (
   <Container>
     <h1>About Me</h1>
-    <p>This is some personal information about myself, including my background, education, and work experience.</p>
+    <p>Im Christian C. Arguelles, including my background, education, and work experience.</p>
   </Container>
 );
 

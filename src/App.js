f75501d8118bae4f5839c288894a9fa8097f4b73
ChristianArguelles <bonechanbuto@gmail.com>
2024-10-14 08:33:42 +0800

    package and mod in app.js

--- src/App.js
+++ src/App.js
@@ -1,35 +1,34 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { Navbar, Nav } from 'react-bootstrap';
-import Home from './Home';
-import About from './About';
-import Skills from './Skills';
-import Contact from './Contact';
-import SubmissionSuccess from './SubmissionSuccess';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import Skills from './components/Skills';
+import Contact from './components/Contact';
+import SubmissionSuccess from './components/SubmissionSuccess';
 
-const App = () => (
-  <Router>
-    <Navbar bg="light" expand="lg">
-      <Navbar.Brand href="/">My Profile</Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="mr-auto">
-          <Nav.Link href="/">Home</Nav.Link>
-          <Nav.Link href="/about">About</Nav.Link>
-          <Nav.Link href="/skills">Skills</Nav.Link>
-          <Nav.Link href="/contact">Contact</Nav.Link>
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
-    <Switch>
-      <Route exact path="/" component={Home} />
-      <Route path="/about" component={About} />
-      <Route path="/skills" component={Skills} />
-      <Route path="/contact" component={Contact} />
-      <Route path="/submission-success" component={SubmissionSuccess} />
-    </Switch>
-  </Router>
-);
+const App = () => {
+  return (
+    <Router>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <Link className="navbar-brand" to="/">Profile</Link>
+        <div className="collapse navbar-collapse">
+          <ul className="navbar-nav">
+            <li className="nav-item"><Link className="nav-link" to="/">Home</Link></li>
+            <li className="nav-item"><Link className="nav-link" to="/about">About</Link></li>
+            <li className="nav-item"><Link className="nav-link" to="/skills">Skills</Link></li>
+            <li className="nav-item"><Link className="nav-link" to="/contact">Contact</Link></li>
+          </ul>
+        </div>
+      </nav>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/skills" component={Skills} />
+        <Route path="/contact" component={Contact} />
+        <Route path="/submission-success" component={SubmissionSuccess} />
+      </Switch>
+    </Router>
+  );
+};
 
 export default App;
